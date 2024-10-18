1274ebf6a00259264d7e15e5f11df5ae3892cc4a
berkaydisli <berkay.dsli@gmail.com>
2024-10-18 17:47:59 +0300

    Initialize

--- UIKitBaseArchitectureAppDemo/SceneDelegate.swift
+++ UIKitBaseArchitectureAppDemo/SceneDelegate.swift
@@ -16,7 +16,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+//        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        let tabBarController = UITabBarController()
+        
+        let homeNavController = UINavigationController(rootViewController: HomeViewController())
+        let searchNavController = UINavigationController(rootViewController: SearchViewController())
+        let profileNavController = UINavigationController(rootViewController: ProfileViewController())
+        
+        homeNavController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        searchNavController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        profileNavController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 2)
+        
+        tabBarController.viewControllers = [homeNavController, searchNavController, profileNavController]
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- UIKitBaseArchitectureAppDemo/ViewController.swift
+++ UIKitBaseArchitectureAppDemo/ViewController.swift
@@ -6,14 +6,102 @@
 //
 
 import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Home"
+        setupSwiftUIView(ContentView(title: "Home", navigateAction: navigateToDetails))
     }
-
-
+    
+    func navigateToDetails() {
+        navigationController?.pushViewController(DetailsViewController(title: "Home Details"), animated: true)
+    }
 }
 
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search"
+        setupSwiftUIView(ContentView(title: "Search", navigateAction: navigateToDetails))
+    }
+    
+    func navigateToDetails() {
+        navigationController?.pushViewController(DetailsViewController(title: "Search Details"), animated: true)
+    }
+}
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Profile"
+        setupSwiftUIView(ContentView(title: "Profile", navigateAction: navigateToDetails))
+    }
+    
+    func navigateToDetails() {
+        navigationController?.pushViewController(DetailsViewController(title: "Profile Details"), animated: true)
+    }
+}
+
+class DetailsViewController: UIViewController {
+    init(title: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.title = title
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSwiftUIView(DetailsView(title: title ?? "Details"))
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
+
+extension UIViewController {
+    func setupSwiftUIView<Content: View>(_ swiftUIView: Content) {
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.view.frame = view.bounds
+        hostingController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        hostingController.didMove(toParent: self)
+        hostingController.navigationItem.largeTitleDisplayMode = .always
+        hostingController.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+struct ContentView: View {
+    let title: String
+    let navigateAction: () -> Void
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Text("Welcome to \(title)")
+            Button("Go to Details") {
+                navigateAction()
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct DetailsView: View {
+    let title: String
+    
+    var body: some View {
+            Text("This is the \(title) view")
+                .navigationTitle(title)
+    }
+}
+
+#Preview {
+    HomeViewController()
+}
