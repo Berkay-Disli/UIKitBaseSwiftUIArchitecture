ad863a6f6ebc3f482cce30cdf8ac720228b4cadb
berkaydisli <berkay.dsli@gmail.com>
2024-10-18 17:15:03 +0300

    Initial Commit

---
+++ UIKitBaseArchitectureAppDemoTests/UIKitBaseArchitectureAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitBaseArchitectureAppDemoTests.swift
+//  UIKitBaseArchitectureAppDemoTests
+//
+//  Created by BERKAY DISLI on 18.10.2024.
+//
+
+import Testing
+@testable import UIKitBaseArchitectureAppDemo
+
+struct UIKitBaseArchitectureAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
